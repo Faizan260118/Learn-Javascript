5c21494a294b71e27450207945011ec6f73b9d2f
Faizan <alammdfaizan50@outlook.com>
2023-12-12 08:32:35 +0530

    function

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,38 @@
+
+function sayMyName() {
+    console.log("F")
+    console.log("A")
+    console.log("I")
+    console.log("Z")
+    console.log("A")
+    console.log("N")
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result:", result)
+
+
+function loginUserMessage(username = "zara"){
+    // if(username === undefined){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("faizan"))
+console.log(loginUserMessage())
