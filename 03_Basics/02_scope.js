2ea7e2915a842bce49ee529c97223af385d2defc
Faizan <alammdfaizan50@outlook.com>
2023-12-12 09:32:38 +0530

    global/block scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -11,6 +11,46 @@ if (true) {
     
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "faizan"
+
+    function two (){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+    // console.log(username)
+}
+
+// one()
+
+if (true) {
+    const username = "faizan"
+    if (username === "faizan") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username)
+
+
+// ************************************ interesting ************************************//
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
