cb334e5917f0ebfda6db9d92ff4d5d5b56f48b03
Faizan <alammdfaizan50@outlook.com>
2023-12-13 20:19:41 +0530

    IIFE.js

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) =>{
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('faizan');
+
+let val1 = 10
+let val2 = 5
+function addNum(num1, num2){
+    let total = num1 + num2
+    return total
+};
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+console.log(typeof addNum());
+// console.log(val1);
