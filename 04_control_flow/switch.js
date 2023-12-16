865230747b8c91a9e185dc0be9fac08c78b9ee1e
Faizan <alammdfaizan50@outlook.com>
2023-12-16 17:34:22 +0530

    control-flow/falsy-truthy

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("Jannuary");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
