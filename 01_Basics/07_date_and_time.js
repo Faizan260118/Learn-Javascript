254aea5aa58fccd35f9c26513f89464a2d66d243
Faizan <alammdfaizan50@outlook.com>
2023-12-17 22:13:07 +0530

    01_Basics

--- 01_Basics/07_date_and_time.js
+++ 01_Basics/07_date_and_time.js
@@ -1,9 +1,16 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
+console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toUTCString());
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
