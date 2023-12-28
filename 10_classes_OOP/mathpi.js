fa8f2eea21834a2cb3ccb238d8abd31ac518ed64
Faizan <alammdfaizan50@outlook.com>
2023-12-28 23:35:28 +0530

    Classes/Objects

---
+++ 10_classes_OOP/mathpi.js
@@ -0,0 +1,29 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descriptor);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailabe: true,
+
+    orderChai: function(){
+        console.log("chai nji bani");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    writable: false,
+    enumerable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
