e2d37f990113a0af9a30b53a97c53a03908e101c
Faizan <alammdfaizan50@outlook.com>
2023-12-28 22:16:53 +0530

    StaticGroup

---
+++ 10_classes_OOP/staticgroup.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const faizan = new User("faizan")
+// console.log(faizan.createId());
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "iphone@.com")
+console.log(iphone.createId());
