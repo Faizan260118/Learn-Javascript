f26d1faee88faeca71ef9f332f7f6f3ec9f4bcf4
Faizan <alammdfaizan50@outlook.com>
2024-02-28 21:37:20 +0530

    control flow of javascript

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,9 +2,9 @@
 const isUserLoggedIn = true
 const temperature = 41
 
-if ( temperature === 40){
+if (temperature === 40) {
     console.log("less than 50");
-}else {
+} else {
     console.log("temperature is greater than 50")
 }
 
@@ -18,29 +18,29 @@ if ( temperature === 40){
 //     console.log(`User power: ${power}`)
 // }
 
-// console.log(`User power: ${power}`)
+// console.log(`User power: ${power}`)  //? Power doesn't     print because of scope
 
 
-// const balance = 1000
+const balance = 1000
 
 //? if (balance >500) console.log("test"),console.log("test2");   unreadable code  
 
-// if (balance < 500) {
-//     console.log("less than");
-// } else if (balance < 750) {
-//     console.log("less than 750");
-// } else if (balance < 900) {
-//     console.log("less than 900")
-// } else {
-//     console.log("less than 1200")
-// }
+if (balance < 500) {
+    console.log("Balance is less than 500");
+} else if (balance < 750) {
+    console.log("Balance is less than 750");
+} else if (balance < 900) {
+    console.log("Balance is less than 900")
+} else {
+    console.log("Balnace is less than 1200")
+}
 
 const userLoggedIn = true
 const debitCard = true
 const loggedInFromGoogle = false
 const loggedInFromEmail = true
 
-if (userLoggedIn && debitCard && 2==2) {
+if (userLoggedIn && debitCard && 2 === 2) {
     console.log("Allow to buy course")
 }
 
