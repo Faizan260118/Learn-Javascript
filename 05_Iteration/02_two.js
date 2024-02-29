e12a84648e4df03892bc198cb5df39e7a3d0539d
Faizan <alammdfaizan50@outlook.com>
2024-02-29 21:31:17 +0530

    while and Do while

--- 05_Iteration/02_two.js
+++ 05_Iteration/02_two.js
@@ -2,7 +2,7 @@
 let index = 0
 // while (index <= 10) {
 //     console.log(` value of index is ${index}`);
-//     index = index +2
+//     index = index + 2
 // }
 
 let myArray = ["flash", "batman", "superman"]
