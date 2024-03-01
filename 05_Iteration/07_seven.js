15478d170cc2829aa6075a944f1735c80684eeeb
Faizan <alammdfaizan50@outlook.com>
2024-03-01 21:24:16 +0530

    seven.js

--- 05_Iteration/07_seven.js
+++ 05_Iteration/07_seven.js
@@ -3,8 +3,8 @@ const myNUmbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNUmbers.map( (num) => {return num +10} )
 
 const newNums = myNUmbers
-.map((num) => num *10)
-.map((num) => num + 5)
-.filter( (num) => num >= 30 )
+    .map((num) => num * 10)
+    .map((num) => num + 5)
+    .filter((num) => num >= 30)
 
 console.log(newNums);
