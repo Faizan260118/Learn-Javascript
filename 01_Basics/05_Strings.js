29ca368baca2313ffd5b90f1548f9e0bc2ff6b57
Faizan Ali <alammdfaizan50@outlook.com>
2024-08-22 21:12:33 +0530

    Update 05 file

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  
 
-const gameName = new String("faizzzan")
+const gameName = new String("fai-zz-zan")
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
