ad9b698c1f228ec316c741d5f43686dffc00834a
Faizan Ali <alammdfaizan50@outlook.com>
2025-02-25 16:11:41 +0530

    check the typeof of null and undefined with numeric values

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -8,19 +8,22 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null < 0);
-console.log(null == 0);
-console.log(null === 0);
-console.log(null >= 0);
-console.log(null <= 0);
+console.log(null > 0);  // false
+console.log(null < 0);  // false
+console.log(null == 0); // false
+console.log(null === 0); // false
+console.log(null >= 0);  // true
+console.log(null <= 0);  // true
 
-console.log(undefined > 0);
-console.log(undefined < 0);
-console.log(undefined == 0);
-console.log(undefined === 0);
-console.log(undefined >= 0);
-console.log(undefined <= 0);
+console.log(undefined > 0);  // false
+console.log(undefined < 0);  // false
+console.log(undefined == 0); // false
+console.log(undefined === 0); // false
+console.log(undefined >= 0);  // false
+console.log(undefined <= 0);  // false
+
+//* Special case */
+console.log(undefined == null); //true
 
 // * We have to avoid these type of conversion * //
 // * Priority to write clean and readable code *//
