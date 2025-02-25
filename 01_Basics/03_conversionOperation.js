9136d52ca71a4eb414115149eca64e0899d8e2c2
Faizan Ali <alammdfaizan50@outlook.com>
2025-02-25 15:13:49 +0530

    Add comment of checking number values

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -61,6 +61,11 @@ let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
 let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
