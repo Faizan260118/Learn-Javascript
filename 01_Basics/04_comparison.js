055ab62297aae73bef6b6bceac88f514bf5c9233
Faizan Ali <alammdfaizan50@outlook.com>
2025-02-25 15:26:17 +0530

    compare the numeric values with null and undefined

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -9,13 +9,18 @@
 // console.log("02" > 1);
 
 console.log(null > 0);
+console.log(null < 0);
 console.log(null == 0);
+console.log(null === 0);
 console.log(null >= 0);
+console.log(null <= 0);
 
-console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined === 0);
 console.log(undefined >= 0);
+console.log(undefined <= 0);
 
 // * We have to avoid these type of conversion * //
 // * Priority to write clean and readable code *//
