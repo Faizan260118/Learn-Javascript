b1e856eb59af97486f7f5ffb1363e1806c6b4975
Faizan Ali <alammdfaizan50@gmail.com>
2025-03-20 01:49:24 +0530

    Modify the if statement

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,49 +1,50 @@
 // if 
-const isUserLoggedIn = true
-const temperature = 41
+const isUserLoggedIn = true;
+const temperature = 41;
 
 if (temperature === 40) {
-    console.log("less than 50");
+    console.log("less than 40");
 } else {
-    console.log("temperature is greater than 50")
-}
+    console.log("temperature is greater than 40");
+};
 
-// console.log("execute")
+// console.log("execute");
 // <, >, <=, >=, ==, !=, ===
 
-// const score = 200
+// const score = 200;
 
 // if (score > 100) {
-//     const power = "fly"
-//     console.log(`User power: ${power}`)
-// }
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// };
 
-// console.log(`User power: ${power}`)  //? Power doesn't     print because of scope
+// console.log(`User power: ${power}`);  //? Power doesn't     print because of scope
 
 
-const balance = 1000
+const balance = 850;
 
-//? if (balance >500) console.log("test"),console.log("test2");   unreadable code  
+//? if (balance >500) console.log("test"),console.log("test2");   unreadable code / Implicit scope
 
-if (balance < 500) {
+if (balance <= 500) {
     console.log("Balance is less than 500");
-} else if (balance < 750) {
+} else if (balance <= 750) {
     console.log("Balance is less than 750");
-} else if (balance < 900) {
-    console.log("Balance is less than 900")
+} else if (balance <= 900) {
+    console.log("Balance is less than 900");
 } else {
-    console.log("Balnace is less than 1200")
-}
+    console.log("Balnace is less than 1200");
+};
 
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+const guestUser = true;
 
 if (userLoggedIn && debitCard && 2 === 2) {
-    console.log("Allow to buy course")
-}
+    console.log("Allow to buy course");
+};
 
 if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("user logged in");
-}
+    console.log("User logged in");
+};
